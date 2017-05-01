cf2b4dd6f5de44b22526e268f7396c4ee5dbd406
teeoorr <tor_26419@hotmail.com>
2017-05-01 23:29:29 +0700

    hello

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
 	bodyParser = require('body-parser')
   
 var We=[];
-
+var We1=[];
 app.use(express.static('public')) 
 
 function randomString(length) {
@@ -24,9 +24,50 @@ router.route('/wes1/:booking_id')
 	res.json({});
 	})
 
-router.route('/wes2') 
+router.route('/wes') 
 	.get( function(req,res) {
 		res.json(We);
+	})
+
+	.post(function(req, res) { 
+	    var we = {}; 
+	    we.name = req.body.name
+	    we.number = req.body.number
+	    we.telephone = req.body.telephone
+		we.email = req.body.email
+	    we.adult = req.body.adult
+	    we.conlist = req.body.conlist
+	    we.booking = req.body.price
+	    we.booking.bookingnumber = randomString(10)
+	    we.process = 'Processing'
+	    we.total = req.body.adult*req.body.price.price
+	    We.push(we); 
+	    res.json({bookingnumber:we.booking.bookingnumber,
+	    					name:we.name,
+	    					total:we.total,
+	    				    adult:we.adult})
+	})
+
+router.route('/wes2') 
+	.get( function(req,res) {
+		res.json(We1);
+	})
+
+	.post(function(req, res) { 
+	    var we1 = {}; 
+	    we1.book1 = req.body.book1
+	    we1.name1 = req.body.name1
+	    we1.price1 = req.body.price1
+		we1.transfer1 = req.body.transfer1
+	    we1.date1 = req.body.date1
+	    we1.banklist1 = req.body.banklist1
+	    We1.push(we1); 
+	    res.json({book1:we1.book1,
+	    		name1:we1.name1,
+	    		price1:we1.price1,
+	    		transfer:we1.transfer1,
+				date1:we1.date1,
+ 				banklist:we1.banklist1 })
 	})
 
 router.route('/wes') 
